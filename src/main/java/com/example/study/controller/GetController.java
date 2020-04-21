3eb7cc64a7faaa5c5494af67ff58be450a67c942
chjin <seedevk8s@gmail.com>
2020-04-21 14:13:19 +0900

    주소 창에 파라미터가 노출 된다

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,8 +1,6 @@
 package com.example.study.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")     // localhost:8080/api
@@ -12,5 +10,13 @@ public class GetController {
     public String getRequest() {
 
         return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter")        // localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam String password) {
+        System.out.println("id: " +id);
+        System.out.println("password: " +password);
+
+        return id+password;
     }
 }
