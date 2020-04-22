3803cdfdd25403d4f57fda4dd9439d629e4ae790
chjin <seedevk8s@gmail.com>
2020-04-22 20:54:10 +0900

    OrderDetailRepository에 대한 create 테스트

---
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.OrderDetail;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class OrderDetailRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    OrderDetailRepository orderDetailRepository;
+
+    @Test
+    public void Create() {
+        OrderDetail orderDetail = new OrderDetail();
+
+        orderDetail.setOrderAt(LocalDateTime.now());
+        //어떤 사람?
+        orderDetail.setUserId(7L);
+        //어떤 상품?
+        orderDetail.setItemId(1L);
+
+        OrderDetail orderDetail1 = orderDetailRepository.save(orderDetail);
+    }
+}
