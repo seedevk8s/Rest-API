8f993fe1dcc211adba951c1202eb4c3d6610cb7d
chjin <seedevk8s@gmail.com>
2020-04-23 15:46:03 +0900

    QueryMetho 하나더 추가함.

--- src/main/java/com/example/study/repository/UserRepository.java
+++ src/main/java/com/example/study/repository/UserRepository.java
@@ -13,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByAccount(String account);         //account에 매칭되는 것을 찾겠다.
 
     Optional<User> findByEmail(String email);            //email로 검색하겠다.
+
+    // select * from user where account = ?  and email = ?
+    // 쿼리를 메서드 형식으로 작성함 =>  QueryMethod라고 함.
+    Optional<User> findByAccountAndEmail(String account, String email);
+
 }
